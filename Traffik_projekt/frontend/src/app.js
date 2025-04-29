07fcfdb59ed5ac9e8bc8568ad9af227f5f4b8208
katjabjorlinger <130973201+katjabjorlinger@users.noreply.github.com>
2025-04-29 13:11:42 +0200

    test för subscription och byggt en modal i flera steg för signup

--- Traffik_projekt/frontend/src/app.js
+++ Traffik_projekt/frontend/src/app.js
@@ -3,12 +3,28 @@ import LoginForm from "./components/LoginForm.js";
 import ResetPasswordForm from "./components/ResetPasswordForm.js";
 //import UpdateProfileForm from "./components/UpdateProfileForm.js";
 const { createApp } = Vue;
+import SubscriptionModal from "./components/SubscriptionModal.js";
+
 
 createApp({
   components: {
     SignupForm,
     LoginForm,
-    ResetPasswordForm,
-    //UpdateProfileForm
+    ResetPasswordForm, 
+    SubscriptionModal
+  },
+  data() {
+    return {
+      modalView: null,
+      showSubscriptionModal: false
+    };
+  },
+  methods: {
+    openModal(view) {
+      this.modalView = view;
+    },
+    closeModal() {
+      this.modalView = null;
+    }
   }
 }).mount("#app");
