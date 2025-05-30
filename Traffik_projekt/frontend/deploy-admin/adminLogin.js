e96c6f0f283f68de4b7b9d5a2e88394cc82d0f3d
h23mlars <h23mlars@du.se>
2025-05-30 14:25:04 +0200

    Add admin-deploy

---
+++ Traffik_projekt/frontend/deploy-admin/adminLogin.js
@@ -0,0 +1,70 @@
+//Login-komponent som hanterar inloggning för tidningar och superadmin. 
+export default {
+    data() {
+        return {
+            email: "",
+            password: "",
+            message: ""
+        };
+    },
+    template: `
+    <div class="container">
+      <h2> Logga in </h2>
+      <form @submit.prevent="login" class="">
+        <div class="form-row">
+        <label>E-post <span class="required">*</span></label>
+         <input v-model="email" type="email" placeholder="E-post"  class="form-field" required />
+         </div>
+         <div class="form-row">
+         <label>Lösenord<span class="required">*</span></label>
+         <input v-model="password" type="password" placeholder="Lösenord" class="form-field" required />
+         </div>
+         <div class="form-row">
+         <button type="submit" class="button-primary"> Logga in </button>
+         </div>
+      </form>
+      <button @click="forgotPassword" style="margin-top: 1rem;" class="button-tertiary">Glömt lösenord?</button>
+      <p v-if="message">{{ message }}</p>
+    </div>
+    `,
+    methods: {
+      //Inloggning - skickar användarens epost och lösenord till backend via en post-request. 
+        async login() {
+            try {
+                const response = await fetch("http://localhost:5000/api/admin/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                   email: this.email,
+                   password: this.password
+                })
+        });
+        
+        const data = await response.json();
+
+        //Om inloggning misslyckas, skriv ut felmeddelande. 
+        if (!response.ok) {
+           this.message = data.error || "Fel vid inloggning";
+           return;
+        }
+
+        // Skicka login-resultat till app.js via event
+        this.$emit("login-success", {
+          access_token: data.access_token,
+          reseller_id: data.reseller_id,
+          role: data.role
+        });
+
+        } catch (error) {
+           this.message = "Ett fel uppstod vid inloggningen.";
+           console.error(error);
+        }
+    },
+    //Metod för glömt lösenord - INTE implementerat!
+    forgotPassword() {
+      this.message = "Lösenordshantering ej implementerad ännu..";
+    }
+}
+    
+}; 
+
---
+++ Traffik_projekt/frontend/deploy-admin/adminPricing.js
@@ -0,0 +1,93 @@
+//hämta återanvändbar hjälpmetod för att göra api-anrop. 
+import {apiFetch} from "../api.js"
+
+
+export default {
+  data() {
+    return {
+      currentPrice: null,
+      newPrice: "",
+      message: "",
+      loading: false //används för att visa laddningsstatus och disable knappar. 
+    };
+  },
+  created() {
+    //När komponenten skapas - anropa funktion för att hämta nuvarande pris. 
+    this.fetchprice();
+
+  },
+  methods: {
+    //Metod för att hämta aktuellt pris för tidningen. 
+    async fetchprice() {
+      this.loading = true; //Visa laddning
+      try {
+        const data = await apiFetch("http://localhost:5000/api/admin/pricing");
+        this.currentPrice = data.price;
+      } catch (err) {
+        console.error("Fel vid hämtning:", err.message);
+        this.message = data.message || "Kunde inte hämta priset";
+      } finally {
+        this.loading = false;
+      }
+    },
+    //rensar formuläret.
+    cancel() {
+      this.newPrice = "";
+      this.message = "";
+    },
+    //Metod för att spara prisändring. 
+    async save() {
+      if (!this.newPrice) {
+        this.message = "Ange ett pris först.";
+        return;
+      }
+
+      try {
+        //skickar nytt pris till backend med POST. 
+        const data = await apiFetch("http://localhost:5000/api/admin/pricing/update", {
+          method: "POST",
+          body: JSON.stringify({ price: parseFloat(this.newPrice)})
+        });
+        //Uppdaterar det aktuella priset och visar bekräftelse.
+        this.currentPrice = this.newPrice;
+        this.message = data.message || "Priset har uppdaterats.";
+        this.newPrice = "";
+
+      } catch (err) {
+        console.error("Fel vid uppdatering: ", err.message);
+        this.message = data.message || "Kunde inte uppdatera priset.";
+      }
+    }
+  },
+  //HTML-struktur för komponenten. 
+  template: `
+    <div class="grid-container">
+      <h1>Prissättning</h1>
+        
+        <div class="">
+          <h3>Nuvarande prissättning</h3>
+          <p v-if="loading">Laddar pris...</p>
+          <p v-else-if="currentPrice !== null">{{ currentPrice }} kr/ månad</p>
+          <p v-else>Inget pris angivet. </p>
+        </div>
+
+        <div class="">
+          <h3>Redigera prissättning</h3>
+          <p class="subtext">SMS-notiser skickas endast vid större trafikpåverkan. Detta är alltid inkluderat.</p>
+        
+        <div class="form-row">
+          <label>Pris per månad:</label>
+          <input v-model="newPrice" type="number" :disabled="loading" placeholder="Ange nytt pris..." />
+        </div>
+
+        <div class="button-group">
+          <button class="button-secondary" @click="cancel" :disabled="loading">Avbryt</button>
+          <button class="button-primary" @click="save" :disabled="loading">Spara</button>
+        </div>
+
+        <p v-if="message">{{ message }}</p>
+        <p v-if="loading">Vänligen vänta...</p>
+      </div>
+    </div>
+  `
+};
