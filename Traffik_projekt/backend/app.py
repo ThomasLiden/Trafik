1956dc39863d7fc8592c80ef5ba6e0f011d8ecfc
h23mlars <h23mlars@du.se>
2025-05-30 12:38:06 +0200

    Remove unnecessary main check and streamline port configuration in app.py

--- Traffik_projekt/backend/app.py
+++ Traffik_projekt/backend/app.py
@@ -32,10 +32,6 @@ def debug():
     from flask import request
     print(f"➡️ {request.method} {request.path}")
 
-if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port=5000)
 
-
-import os
 port = int(os.environ.get("PORT", 5000))
 app.run(debug=True, host="0.0.0.0", port=port)
